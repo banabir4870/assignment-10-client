6e1ca8b7b14251b4d2687f03d7fd287fa212b5fb
banabir4870 <banabir4870@gmail.com>
2026-06-28 07:24:07 +0600

    added top legal expert section

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Banner from "@/components/Banner";
 import FeaturedLawyers from "@/components/FeaturedLawyers";
+import TopLegalExperts from "@/components/TopLegalExperts";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <div>
       <Banner />
       <FeaturedLawyers />
+      <TopLegalExperts />
     </div>
   );
 }
